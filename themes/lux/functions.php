675a8d911a3f22aac00d051ec03edec3b6701076
sabahat <sabahat.hussain@dcmail.ca>
2022-02-14 16:33:18 -0500

    Custom header removed

--- themes/lux/functions.php
+++ themes/lux/functions.php
@@ -150,11 +150,6 @@ function lux_scripts() {
 add_action( 'wp_enqueue_scripts', 'lux_scripts' );
 
 /**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
