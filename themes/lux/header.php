3aabfff83b1292f50586139e7c07395775efd78b
sabahat <sabahat.hussain@dcmail.ca>
2022-03-07 11:33:38 -0500

    Header Created

--- themes/lux/header.php
+++ themes/lux/header.php
@@ -25,28 +25,83 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'lux' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$lux_description = get_bloginfo( 'description', 'display' );
-			if ( $lux_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $lux_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+	<div class="announcement-bar">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<ul class="announcement-bar__list pt-2 pb-2">
+					
+					<li><i class="bi bi-telephone rounded-circle"></i>
+					<a href="tel: 416-785 4323">416-785 4323</a>
+					</li>
 
-		<nav id="site-navigation" class="main-navigation">
-			<?php
+					<li><i class="bi bi-envelope rounded-circle"></i>
+					<a href="mailto:hello@luxstyle.shop">hello@luxstyle.shop</a>
+					</li>
+					
+					</ul>
+			</div>
+			<div class="col-md-8 d-flex justify-content-end">
+			<ul class="announcement-bar__list">
+					
+					<li><i class="bi bi-truck rounded-circle"></i>
+					Free Shipping
+					</li>
+
+					<li><i class="bi bi-clock-history rounded-circle"></i>
+					30 DAYS MONEYBACK GUARANTEE
+					</li>
+
+					<li><i class="bi bi-person rounded-circle"></i>
+					24/7 CUSTOMER SUPPORT
+					</li>
+					
+					</ul>
+			</div>
+		</div>
+	</div>
+	</div>
+</div>
+	<header id="masthead" class="site-header">
+       
+	<div class="container pt-2 pb-2">
+		<div class="row align-items-center">
+
+			<div class="col site-header__logo d-flex justify-content-center justify-content-md-start pb-2">
+			<?php the_custom_logo(); ?>
+			</div>
+
+			<div class="col-sm-12 col-md-5">
+			<?php aws_get_search_form( true ); ?>
+			</div>
+
+			<div class="col cart d-flex justify-content-center justify-content-md-end align-items-center pt-2">
+			
+			<a href="<?php echo wc_get_cart_url(); ?>"><i class="bi bi-bag-dash p-2"></i></a>
+			<a class="cart-customlocation" href="<?php echo wc_get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><?php echo sprintf ( _n( '%d item', '%d items', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() ); ?> – <?php echo WC()->cart->get_cart_total(); ?></a>
+
+			</div>
+	
+	</div>
+	</div>
+
+		
+
+	<nav id="site-navigation" class="main-navigation">
+
+	<div class="container d-flex justify-content-center">
+
+	 <div class="row">
+		 <div class="col-12 d-flex justify-content-center">
+			 <!--<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+			 <i class="bi bi-list"></i> 
+			 <?php esc_html_e( 'Primary Menu', 'luxstyle'); ?>
+			</button>-->
+		 </div>
+
+		 
+	<div class="col-12 text-align-center">
+		 <?php
 			wp_nav_menu(
 				array(
 					'theme_location' => 'menu-1',
@@ -54,5 +109,18 @@
 				)
 			);
 			?>
+		 
+	</div>
+	 	
+</div>
+
+		
+		
+		</div>
 		</nav><!-- #site-navigation -->
+
+
+
+
+
 	</header><!-- #masthead -->
